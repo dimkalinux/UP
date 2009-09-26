eb0ae6aaa987b9d9d654b7be54248afa176d3182
dimkalinux <dimka.linux@gmail.com>
2009-09-26 14:11:43 +0000

    Complete page with addons uploaders

--- service/uploaders.php
+++ service/uploaders.php
@@ -11,11 +11,12 @@ require UP_ROOT.'header.php';
 <h2>Дополнительные программы</h2>
 
 <p>Существуют несколько программ, позволяющих закачивать файлы на&nbsp;наш сервис.
-Мы рекомендуем использовать эти программы для закачки очень больших файлов&nbsp;&mdash; более 2 гигабайт.</p>
+Мы рекомендуем использовать эти программы для закачки очень больших файлов&nbsp;&mdash; более 2&nbsp;гигабайт.</p>
 <ul>
-	<!--<li><a href="http://forum.lluga.net/labs/iskip/">iSkip&nbsp;&mdash; расширение для браузера&nbsp;Фаирфокс</a></li>-->
+	<li><a href="http://forum.lluga.net/labs/fireUP/">fireUP&nbsp;&mdash; расширение для браузера&nbsp;Фаирфокс</a></li>
 	<li><a href="http://forum.lluga.net/labs/sendtoup/">Send to&nbsp;UP&nbsp;&mdash; <acronym title="Practical Extraction and Reporting Language" lang="en">PERL</acronym> скрипт для консольной загрузки&nbsp;файлов</a></li>
 </ul>
+<p>Также можно закачивать файлы <a href="/ftp_access.php">с помощью фтп-клиентов</a>.</p>
 
 
 <?
